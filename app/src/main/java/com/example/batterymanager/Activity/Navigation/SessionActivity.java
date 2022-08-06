f24159e7818093cfe4aeec12128ec23de308d28e
Ashish Saran Shakya <harshshakya18585@gmail.com>
2022-08-06 12:53:39 +0530

    Added activity for session

---
+++ app/src/main/java/com/example/batterymanager/Activity/Navigation/SessionActivity.java
@@ -0,0 +1,14 @@
+package com.example.batterymanager.Activity.Navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session);
+    }
+}
